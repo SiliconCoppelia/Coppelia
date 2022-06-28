b5663bb40cb332781abf14a01f17d6e5bc49c635
BanjiBear <tommychen900217@gmail.com>
2022-06-28 17:26:11 +0800

    Ethics/Affordances update

--- SiliconCoppélia/src/Controller.java
+++ SiliconCoppélia/src/Controller.java
@@ -1,22 +1,13 @@
 import dimensions.*;
 
-import java.awt.*;
-import java.util.*;
 import java.lang.*;
-import javax.swing.*;
-import java.util.Random;
 
 import static java.lang.Integer.parseInt;
 
 public class Controller {
 
-    private static Scanner scan = new Scanner(System.in);
-
-    private static float ETHICS;
-    private static int[] affordanceFeatures = new int[3];
-
     private static String[] ethics = {"smiles", "shows an angry face", "cheats"};
-    private static String[] affordance = {"acts fast", "acts slow", "is accurate", "has low IQ score", "has high IQ score"};
+    private static String[] affordance = {"acts fast", "acts slow", "is accurate", "has high IQ score", "has low IQ score"};
 
     private static StringBuffer sent1 = new StringBuffer("You are ");               // Ethics and Affordances
     private static StringBuffer sent2 = new StringBuffer("I find ");                // Relevance: What is important and what is not
@@ -25,23 +16,25 @@ public class Controller {
 
     public static void main(String[] args){
 
-        // Step 1: greetings from Coppélia
         System.out.println("Hi, I'm Coppélia.\n");
 
-        // Step 2: Random generate Agent feature (Ethics and Affordances)
         /*
             https://www.freecodecamp.org/news/generate-random-numbers-java/
             https://www.baeldung.com/java-generating-random-numbers-in-range
             Java Random int -> Greater or equal to 0 and Less than 1
             The max should not be length - 1, or it will never reach the largest
         */
-        System.out.println("Agency " + ethics[getRandomNumber(0, ethics.length)]);
+        int ethicInd = getRandomNumber(0, ethics.length);
+        int affInd = getRandomNumber(0, affordance.length);
+        System.out.println("Agency " + ethics[ethicInd]);
 
-
-        // Step 3: Create all object and pass parameters for processing
+        Ethics eth = new Ethics(ethicInd, Math.random());
+        Affordance aff = new Affordance(affInd, Math.random());
 
 
         // Step 4: sentence formulation
+        System.out.println(eth.getResponse());
+        System.out.println(aff.getResponse());
 
         System.exit(0);
     }
