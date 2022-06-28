09ea676e4b490ee7832eec07850198a60e0c3de4
BanjiBear <tommychen900217@gmail.com>
2022-06-28 16:45:05 +0800

    Agency feature DONE

--- SiliconCoppélia/src/Controller.java
+++ SiliconCoppélia/src/Controller.java
@@ -14,45 +14,39 @@ public class Controller {
 
     private static float ETHICS;
     private static int[] affordanceFeatures = new int[3];
-    private static int num = 0;
-    private static int index = 0;
-    private static String[] features = {"a friendly appearance ", "a young age ", "a wealthy income status ", "keeping one or two pets "};
-    private static String[] valence = {"Your appearance ", "Your age ", "Your income status ", "The number of pets you have "};
+
+    private static String[] ethics = {"smiles", "shows an angry face", "cheats"};
+    private static String[] affordance = {"acts fast", "acts slow", "is accurate", "has low IQ score", "has high IQ score"};
 
     private static StringBuffer sent1 = new StringBuffer("You are ");               // Ethics and Affordances
     private static StringBuffer sent2 = new StringBuffer("I find ");                // Relevance: What is important and what is not
     private static StringBuffer sent3 = new StringBuffer("");                       // Valence: hope/fear a pos/neg outcome
     private static StringBuffer sent4 = new StringBuffer("Therefore, ");            // Involvement and Distance
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args){
 
         // Step 1: greetings from Coppélia
         System.out.println("Hi, I'm Coppélia.\n");
 
         // Step 2: Random generate Agent feature (Ethics and Affordances)
-        usrIntro();
+        /*
+            https://www.freecodecamp.org/news/generate-random-numbers-java/
+            https://www.baeldung.com/java-generating-random-numbers-in-range
+            Java Random int -> Greater or equal to 0 and Less than 1
+            The max should not be length - 1, or it will never reach the largest
+        */
+        System.out.println("Agency " + ethics[getRandomNumber(0, ethics.length)]);
+
 
         // Step 3: Create all object and pass parameters for processing
-        Random rd = new Random(); ETHICS = rd.nextFloat(); Ethics eth = new Ethics(ETHICS); // Random Ethics
-        Affordance aff = new Affordance(affordanceFeatures);
-        Valence val = new Valence(ETHICS, affordanceFeatures);
-        // feature-1: ethics   feature-2: age  feature-3: income  feature-4: pet
-        // For Users: All relevance can be changed here
-        Relevance[] rel = {new Relevance(0.657), new Relevance(0.9), new Relevance(0.312), new Relevance(0.76)};
-        Involvement invl = new Involvement(ETHICS, aff.convert(affordanceFeatures[0], 0), aff.convert(affordanceFeatures[1], 1), aff.convert(affordanceFeatures[2], 2),  0.75,  0.4);
-        Distance dist = new Distance(ETHICS, aff.convert(affordanceFeatures[0], 0), aff.convert(affordanceFeatures[1], 1), aff.convert(affordanceFeatures[2], 2),  0.25,  0.4);
-        UseIntention useInt = new UseIntention(ETHICS, aff.convert(affordanceFeatures[0], 0), aff.convert(affordanceFeatures[1], 1), aff.convert(affordanceFeatures[2], 2));
+
 
         // Step 4: sentence formulation
 
         System.exit(0);
     }
 
-    /* ----------------------------------------------------------------------------------------------------------------------------------------------- */
-    /* ----------------------------------------------------------------------------------------------------------------------------------------------- */
-    /* ----------------------------------------------------------------------------------------------------------------------------------------------- */
-
-    private static void usrIntro(){
-        
+    private static int getRandomNumber(int min, int max) {
+        return (int)((Math.random() * (max - min)) + min);
     }
 }
